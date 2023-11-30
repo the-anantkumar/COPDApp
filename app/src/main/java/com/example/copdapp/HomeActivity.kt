55ecffb8e8de8b783f3bb40d08ef1dd65cdbfec7
the-anantkumar <f20191046@hyderabad.bits-pilani.ac.in>
2023-12-01 04:47:25 +0530

    adding materialdesign 3, and app crashing after login

--- app/src/main/java/com/example/copdapp/HomeActivity.kt
+++ app/src/main/java/com/example/copdapp/HomeActivity.kt
@@ -1,54 +1,80 @@
-
 package com.example.copdapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        Toast.makeText(this, "Welcome to COPD App!", Toast.LENGTH_SHORT).show()
+        // Initialize the Toolbar
+        val toolbar: Toolbar = findViewById(R.id.toolbar) // Make sure you have a Toolbar with this ID in your layout
+        setSupportActionBar(toolbar)
 
-        findViewById<Button>(R.id.btnDashboard).setOnClickListener {
-            //switch to the dashboard fragment
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.nav_view)
+
+        // Set up the ActionBarDrawerToggle, which calls the Toolbar
+        toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Sync the state of the drawer indicator/affordance with the linked DrawerLayout
+        toggle.syncState()
+
+        // Set up default fragment
+        if (savedInstanceState == null) {
             navigateToFragment(DashboardFragment())
         }
 
-        findViewById<Button>(R.id.btnMedicalAdherence).setOnClickListener {
-            // Navigate to Medical Adherence
-           navigateToFragment(MedicationAdherenceFragment())
-        }
-
-        findViewById<Button>(R.id.btnCOPDAwareness).setOnClickListener {
-            // Navigate to COPD Awareness Activity or show COPD Awareness content
-            navigateToFragment(COPDAwarenessFragment())
-        }
-
-        findViewById<Button>(R.id.btnCOPDQuestionnaire).setOnClickListener {
-            // Navigate to COPD Questionnaire Activity or show COPD Questionnaire content
-            navigateToFragment(COPDQuestionnaireFragment())
-        }
-
-        findViewById<Button>(R.id.btnWalkTest).setOnClickListener {
-            // Navigate to Walk Test Game Activity or show Walk Test Game content
-        }
-
-        findViewById<Button>(R.id.btnCommunityChat).setOnClickListener {
-            // Navigate to Community Chat Activity or show Community Chat content
+        // Set up navigation item selection
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawer(GravityCompat.START)
+            when (menuItem.itemId) {
+                R.id.nav_dashboard -> navigateToFragment(DashboardFragment())
+                R.id.nav_medical_adherence -> navigateToFragment(MedicationAdherenceFragment())
+                // Add cases for other menu items
+            }
+            true
         }
     }
-    private fun navigateToFragment(fragment: androidx.fragment.app.Fragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
+
+    private fun navigateToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.content_frame, fragment)
+            .commit()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
-
--- app/src/main/java/com/example/copdapp/MainActivity.kt
+++ app/src/main/java/com/example/copdapp/MainActivity.kt
@@ -2,16 +2,20 @@ package com.example.copdapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.android.material.button.MaterialButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var emailEditText: TextInputEditText
+    private lateinit var passwordEditText: TextInputEditText
+    private lateinit var loginButton: MaterialButton
+    private lateinit var signUpTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,37 +24,59 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = FirebaseAuth.getInstance()
 
-        val emailEditText = findViewById<EditText>(R.id.emailEditText)
-        val passwordEditText = findViewById<EditText>(R.id.passwordEditText)
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        val signUpTextView = findViewById<TextView>(R.id.signUpTextView)
+        // Initialize UI components
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        loginButton = findViewById(R.id.loginButton)
+        signUpTextView = findViewById(R.id.signUpTextView)
 
+        // Set up button click listeners
+        setUpClickListeners()
+    }
+
+    private fun setUpClickListeners() {
         loginButton.setOnClickListener {
-            val email = emailEditText.text.toString()
-            val password = passwordEditText.text.toString()
-            signIn(email, password)
+            val email = emailEditText.text.toString().trim()
+            val password = passwordEditText.text.toString().trim()
+            if (validateInputs(email, password)) {
+                signIn(email, password)
+            }
         }
 
         signUpTextView.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            navigateToSignUp()
         }
+    }
+
+    private fun validateInputs(email: String, password: String): Boolean {
+        // You can add more sophisticated validation if needed
+        return email.isNotEmpty() && password.isNotEmpty()
     }
 
     private fun signIn(email: String, password: String) {
         auth.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    // Sign-in success, navigate to HomeActivity
-                    Toast.makeText(baseContext, "Login Successful", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, HomeActivity::class.java)
-                    startActivity(intent)
-                    finish() // Close the current activity
+                    navigateToHome()
                 } else {
-                    // If sign-in fails, display a message to the user
-                    Toast.makeText(baseContext, "Authentication failed.",
-                        Toast.LENGTH_SHORT).show()
+                    displayErrorMessage()
                 }
             }
+    }
+
+    private fun navigateToHome() {
+        Toast.makeText(baseContext, "Login Successful", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish() // Close the current activity
+    }
+
+    private fun navigateToSignUp() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun displayErrorMessage() {
+        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
     }
 }
