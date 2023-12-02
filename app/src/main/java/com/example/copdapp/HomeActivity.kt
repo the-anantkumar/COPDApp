b58f6a3bd699a440c44b6954a911082aca1a05b7
the-anantkumar <f20191046@hyderabad.bits-pilani.ac.in>
2023-12-03 05:07:29 +0530

    fragments working but cannot reach fragment from drawer menu

--- app/src/main/java/com/example/copdapp/HomeActivity.kt
+++ app/src/main/java/com/example/copdapp/HomeActivity.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.DialogTitle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 
+@Suppress("DEPRECATION")
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
@@ -80,6 +80,7 @@ class HomeActivity : AppCompatActivity() {
         return true
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
